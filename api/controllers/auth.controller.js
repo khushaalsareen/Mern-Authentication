3d7b0b74c827e7c361a2646d42fb34b4b615fa3c
khushaalsareen <khushaalsareen@gmail.com>
2024-03-27 10:47:16 +0530

    error handling middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,30 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req,res)=>{
-  const {username, email, password} = req.body;
-  const hashedPassword = bcryptjs.hashSync(password,10);
-  const newUser = new User({username,email,password:hashedPassword});
+export const signup = async (req,res,next)=>{
   try{
+  const {username, email, password} = req.body;
+  //Missing Details
+  if (!username || !email || !password) {
+    return res.status(400).json({ success: false, message: "Missing required fields" });
+  }
+  // Validate email format
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ success: false, message: "Invalid email format" });
+  }
+  const hashedPassword = await bcryptjs.hash(password,10);
+  const newUser = new User({username,email,password:hashedPassword});
     await newUser.save();
     return res.status(200).json({
       message: 'User created successfully',
     })
   } catch(error){
-    return res.status(500).json(error.message);
+    next(error);
   }
+}
+
+function isValidEmail(email) {
+  // Regular expression for basic email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
