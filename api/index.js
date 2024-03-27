958a6d1615abca20248b314bd871a6f00ea78ad7
khushaalsareen <khushaalsareen@gmail.com>
2024-03-27 10:15:40 +0530

    sign-up api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB');
@@ -13,8 +14,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
 })
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
