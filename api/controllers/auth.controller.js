9541c32c4ffc6b0eb24600d23d2d20f64922c77c
khushaalsareen <khushaalsareen@gmail.com>
2024-03-27 12:58:42 +0530

    sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup = async (req,res,next)=>{
   try{
@@ -22,6 +24,32 @@ export const signup = async (req,res,next)=>{
   } catch(error){
     next(error);
   }
+}
+
+export const signin = async (req,res,next)=>{
+  try{
+    const {email, password} = req.body;
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,"No such user exists"))
+    }
+    const validPassword = await bcryptjs.compare(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(401,"Invalid Credentials"))
+    }
+   const token = jwt.sign({id:validUser._id}, process.env.JWT__SECRET)
+   const {password:hashedPassword, ...rest} = validUser._doc;
+   const expiryDate = new Date(Date.now() + 3600000) // 1hour
+   res.cookie('access_token',token,{httpOnly:true, expires: expiryDate}).status(200).json({
+    success: true,
+    message: 'Login Success',
+    rest
+   })
+  }
+  catch(error){
+    next(error);
+  }
+
 }
 
 function isValidEmail(email) {
